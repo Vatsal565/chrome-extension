3df6b10ca433baf00782bc913ca5c609610416ec
Vatsal565 <vatsalbateriwala562005@gmail.com>
2024-06-03 14:49:17 +0530

    local-storage-setup

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-let myLeads = []
+let myLinks = []
+
 const inputEl = document.querySelector("#input-el")
 const inputbtn = document.querySelector('#input-btn')
 const ulEl = document.querySelector("#ul-el")
 
-inputbtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
+let stored_links = JSON.parse(localStorage.getItem("myLinks"))
+
+if (stored_links) {
+    myLinks = stored_links
     renderCode()
+}
+
+inputbtn.addEventListener("click", function() {
+    myLinks.push(inputEl.value)
+    inputEl.value = ""
+
+    localStorage.setItem("myLinks", JSON.stringify(myLinks))
+    renderCode()
+    console.log(localStorage.getItem("myLinks"));
 })
+
+// console.log(localStorage.getItem("myLinks"));
+// local storage only stores strings
+// therefore we have to convert the array into strings using JSON.stringify()
+// to fatch data from the storage, use JSON.parse(localStorage.getItem(key))
 
 function renderCode() {
     let listItems = ""
     
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < myLinks.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${myLinks[i]}'>
+                    ${myLinks[i]}
                 </a>
             </li>`
         /*  
         adding by creating a new element and then set text content, then append it to ul.
         
         const li = document.createElement("li")
-        li.textContent = myLeads[i];
+        li.textContent = myLinks[i];
         ulEl.append(li)
         */
     }
